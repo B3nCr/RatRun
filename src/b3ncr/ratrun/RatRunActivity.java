dd090841f828d71b460c5c7762d7f9e3457c0b09
unknown <ben.crinion@gmail.com>
2012-02-18 13:40:58 +0000

    initial android project structure

---
+++ src/b3ncr/ratrun/RatRunActivity.java
@@ -0,0 +1,13 @@
+package b3ncr.ratrun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RatRunActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
